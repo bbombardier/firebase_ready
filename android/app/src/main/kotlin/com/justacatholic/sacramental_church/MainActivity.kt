04c37fe84b8a3de6e05ed76c5ee896a683e6c815
bbombardier <bryan.bombardier@protonmail.com>
2021-02-20 11:08:24 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/justacatholic/sacramental_church/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.justacatholic.sacramental_church
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) return Container();
+        if (snapshot.connectionState == ConnectionState.done) return Container();
+        return CircularProgressIndicator();
+      }
+    );
+  }
+}
